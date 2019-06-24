26405d0e93c69eae88c3983cf89aeccf1cd31793
Pradip Hamal <pradip.hamal31@gmail.com>
2019-06-24 15:50:47 +0545

    Added routing for customer component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { CustomerComponent } from './customer/components/customer/customer.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'customers', component: CustomerComponent }
 ];
 
 @NgModule({
