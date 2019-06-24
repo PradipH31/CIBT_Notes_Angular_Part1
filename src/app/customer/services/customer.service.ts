e3e90a35c113b72a9d44e1eaa28dbe16b1643e8d
Pradip Hamal <pradip.hamal31@gmail.com>
2019-06-24 15:39:57 +0545

    Set up service

--- src/app/customer/services/customer.service.ts
+++ src/app/customer/services/customer.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Customer } from '../models/customer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
+
+  private customerList: Array<Customer> = new Array<Customer>();
+
+  public getAll(): Array<Customer> {
+    return this.customerList;
+  }
+
+  public save(customer: Customer): void {
+    this.customerList.push(customer);
+  }
 
   constructor() { }
 }
