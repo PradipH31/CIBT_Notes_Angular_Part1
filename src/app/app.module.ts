68daa78f8be27e5c766ef2cad223ac70fa1074f0
Pradip Hamal <pradip.hamal31@gmail.com>
2019-06-24 14:52:29 +0545

    Started fresh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { AdboxComponent } from './adbox/adbox.component';
-import { PropertyManagerModule } from './property-manager/property-manager.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    AdboxComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PropertyManagerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
