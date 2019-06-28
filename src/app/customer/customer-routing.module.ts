70d7eb07ccdc8d12b0604a752e1af7b7c49a439e
Pradip Hamal <pradip.hamal31@gmail.com>
2019-06-28 14:39:40 +0545

    Set up customer routing module

---
+++ src/app/customer/customer-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CustomerComponent } from './components/customer/customer.component';
+import { CustomerDetailComponent } from './components/customer-detail/customer-detail.component';
+
+const routes: Routes = [
+    { path: '', component: CustomerComponent },
+    { path: 'detail/:id', component: CustomerDetailComponent }
+];
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class CustomerRoutingModule { }
