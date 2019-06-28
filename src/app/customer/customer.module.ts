d79f8608166fc819b890488b6577da6334c3ffbe
Pradip Hamal <pradip.hamal31@gmail.com>
2019-06-28 17:29:11 +0545

    Generated customer-add component

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { CustomerComponent } from './components/customer/customer.component';
 import { CustomerDetailComponent } from './components/customer-detail/customer-detail.component';
 import { CustomerRoutingModule } from './customer-routing.module';
+import { CustomerAddComponent } from './components/customer-add/customer-add.component';
 
 @NgModule({
-  declarations: [CustomerComponent, CustomerDetailComponent],
+  declarations: [CustomerComponent, CustomerDetailComponent, CustomerAddComponent],
   imports: [
     CommonModule,
     CustomerRoutingModule
