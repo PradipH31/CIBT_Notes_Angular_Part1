0c5bb7791b08551c9d7bcd9bd6d3d03443eb7202
Pradip Hamal <pradip.hamal31@gmail.com>
2019-06-28 18:12:33 +0545

    Set form as customerForm(formGroup) from component.ts file & imported FormsModule and ReactiveFormsModule

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -4,12 +4,15 @@ import { CustomerComponent } from './components/customer/customer.component';
 import { CustomerDetailComponent } from './components/customer-detail/customer-detail.component';
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerAddComponent } from './components/customer-add/customer-add.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [CustomerComponent, CustomerDetailComponent, CustomerAddComponent],
   imports: [
     CommonModule,
-    CustomerRoutingModule
+    CustomerRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
     CustomerRoutingModule
