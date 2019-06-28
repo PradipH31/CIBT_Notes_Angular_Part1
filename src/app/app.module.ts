bb4015517e2e177b76739ed619ea3dc81fcc5431
Pradip Hamal <pradip.hamal31@gmail.com>
2019-06-28 14:03:11 +0545

    Generated logout component in auth/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { LoginComponent } from './auth/login/login.component';
 import { CustomerModule } from './customer/customer.module';
-import { FormsModule } from "@angular/forms"
+import { FormsModule } from "@angular/forms";
+import { LogoutComponent } from './auth/logout/logout.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     LoginComponent,
+    LogoutComponent,
   ],
   imports: [
     BrowserModule,
