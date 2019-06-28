5977e0ac21ec38b67214cd8d323c38d85b156de4
Pradip Hamal <pradip.hamal31@gmail.com>
2019-06-28 14:41:18 +0545

    Imported & exported CustomerRoutingModule

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -3,15 +3,18 @@ import { CommonModule } from '@angular/common';
 import { CustomerComponent } from './components/customer/customer.component';
 import { RouterModule } from '@angular/router';
 import { CustomerDetailComponent } from './components/customer-detail/customer-detail.component';
+import { CustomerRoutingModule } from './customer-routing.module';
 
 @NgModule({
   declarations: [CustomerComponent, CustomerDetailComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    CustomerRoutingModule
   ],
   exports: [
-    CustomerComponent
+    CustomerComponent,
+    CustomerRoutingModule
   ]
 })
 export class CustomerModule { }
