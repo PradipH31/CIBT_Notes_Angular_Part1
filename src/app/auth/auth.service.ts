0910597453d90aa908bbb9fb5a5f0c78ed7ef8b8
Pradip Hamal <pradip.hamal31@gmail.com>
2019-06-28 13:29:52 +0545

    Created login function

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,7 +5,22 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
 
+  users = [
+    { username: 'admin', password: 'admin' },
+    { username: 'user', password: 'admin' }
+  ]
+
   constructor() { }
+
+  login(username: string, password: string): boolean {
+    for (let i = 0; i < this.users.length; i++) {
+      let user = this.users[i];
+      if (user.username === username && user.password === password) {
+        return true;
+      }
+    }
+    return false;
+  }
 
   loggedInUser(): boolean {
     if (!!localStorage.getItem('token')) {
