b31c57a52eb7556c66cb462fd3c7e0dd034db5c4
Pradip Hamal <pradip.hamal31@gmail.com>
2019-07-01 15:38:37 +0545

    Added multiple event listeners

--- src/app/helpers/highlighter.directive.ts
+++ src/app/helpers/highlighter.directive.ts
@@ -6,12 +6,29 @@ import { ElementSchemaRegistry } from '@angular/compiler';
 })
 export class HighlighterDirective {
 
+  color = 'white';
+  backgroundColor = 'green';
+
   constructor(private el: ElementRef) {
-    el.nativeElement.style.backgroundColor = 'green';
+    this.el.nativeElement.style.backgroundColor = this.backgroundColor;
   }
 
   @HostListener('click') onclick() {
-    this.el.nativeElement.style.color = 'white';
+    this.changeColor();
+  }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.color = 'red';
+    this.changeColor();
+  }
+
+  @HostListener('mouseleave') onmouseleave() {
+    this.color = 'white';
+    this.changeColor();
+  }
+
+  changeColor() {
+    this.el.nativeElement.style.color = this.color;
   }
 
 }
